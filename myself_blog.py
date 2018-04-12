8dd054f116248324405e8c78ab54810e4d7ee06a
Stranger115 <1373616834@qq.com>
2018-04-12 14:34:48 +0800

    first commit

--- myself_blog.py
+++ myself_blog.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import abort
 
 app = Flask(__name__)
 
@@ -6,15 +7,27 @@ app = Flask(__name__)
 # 处理url和函数之间关系的程序叫做路由（装饰器）
 @app.route('/')
 def hello_world():
-    """视图函数"""
-    return 'Hello World!'
+    """视图函数，默认返回200"""
+    # Flask使用上下文让特定变量在一个线程中可全局访问，而不干扰其他线程
+    # user_agent = request.headers.get('User_Agent')
+    return render_template('index.html')
 
 
 # 动态响应 flask 支持路由中使用int、float、path
 # 例如：/user/<int :id>:则动态片段只匹配id为整形的url
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html', name=name)
+
+
+# @app.route('/user/<id>')
+# def get_user(id):
+#     load_user = {2: 'adada'}
+#     user = load_user(id)
+#     print(user)
+#     if not user:
+#         abort(404)  # 抛出异常把控制权交给web服务器
+#     return render_template('user.html', name=user)
 
 
 if __name__ == '__main__':
